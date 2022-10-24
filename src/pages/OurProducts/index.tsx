1603739b843fc5ecaf8d7074bcf0273c8ad4b514
Fernanda Rabaçal <nandarabacal02@hotmail.com>
2022-10-24 13:44:21 -0300

    setting new header on fetch

--- src/pages/OurProducts/index.tsx
+++ src/pages/OurProducts/index.tsx
@@ -16,7 +16,9 @@ export function OurProducts(){
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods" : "GET, POST",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       }
     })
     .then(response => response.json())
