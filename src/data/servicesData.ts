c521a6c2d0f078392973f80c85a8f7bbb3d9a290
Fernanda Rabaçal <nandarabacal02@hotmail.com>
2022-11-30 19:16:05 -0300

    refactor: service cards on service page

---
+++ src/data/servicesData.ts
@@ -0,0 +1,23 @@
+export const servicesData = [
+  {
+    "id": "1A",
+    "name": "Tosa",
+    "description": "Os melhores profissionais e os melhores produtos para deixar o seu animalzinho com o pelo maravilhoso!",
+    "category": "petshop",
+    "image": "https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2020/05/cachorro-sendo-tosado.jpg?w=626&ssl=1"
+  },
+  {
+    "id": "1B",
+    "name": "Banho",
+    "description": "Com os shampoos dos nossos parceiros, seu cãozinho vai sair super cheiroso e com o pelo brilhante! Possuímos vários tipos de banhos, incluindo corte de unha e limpeza de ouvidos.",
+    "category": "petshop",
+    "image": "https://media.istockphoto.com/id/1277453154/pt/foto/golden-retriever-dog-in-a-grooming-salon-is-taking-a-shower.jpg?s=612x612&w=0&k=20&c=Nt-s42_fau56I_4QcrMt_kDZACuNaUcCQ1KOORjNQwg="
+  },
+  {
+    "id": "1C",
+    "name": "Corte de unha",
+    "description": "Junto com o corte de unha, fazemos também a limpeza de ouvido, com todo o cuidado para não machucar o seu pet.",
+    "category": "petshop",
+    "image": "https://media.istockphoto.com/id/514772673/pt/foto/mulher-%C3%A9-cortar-as-unhas-de-c%C3%A3o.jpg?s=612x612&w=0&k=20&c=grYCkKiSkmJxEKnLpcPZSNPWrMlZ_Y5VthBVaL0inRs="
+  },
+]
