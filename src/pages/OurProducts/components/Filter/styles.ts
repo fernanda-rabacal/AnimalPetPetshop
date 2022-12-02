197d8146e1866cccda4c1174a7299ef3c5a6036d
Fernanda Rabaçal <nandarabacal02@hotmail.com>
2022-12-02 13:40:23 -0300

    fix: adjusting filter container and pagination position

--- src/pages/OurProducts/components/Filter/styles.ts
+++ src/pages/OurProducts/components/Filter/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const FilterContainer = styled.aside`
   grid-area: A;
+  width: 30%;
   display: flex;
   flex-direction: column;
 
@@ -27,7 +28,6 @@ export const FilterContainer = styled.aside`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 25%;
     margin-bottom: 2rem;
     border-bottom: 1px solid transparent;
 
